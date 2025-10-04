07be0231efed8309127152c9a76dcfdced7e3457
JD010499 <campi010499@gmail.com>
2025-10-03 20:00:06 -0500

    cambio de Juan

--- lab_git.py
+++ lab_git.py
@@ -3,6 +3,7 @@ import time
 
 def info():
     time.sleep(5)
+    print("Procesando solicitud...")
     return json.dumps({"message": "Hola Mundo", "userChange": "Juan David Campiño"})
 
 print(info())
